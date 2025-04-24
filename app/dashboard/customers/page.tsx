e43847621f9f65901a2f07ee45c862c4c57b5c2e
y-tsujikawa <tsujikawa.yuri.infratop@gmail.com>
2025-04-24 13:26:07 +0900

    [Fix] ESLintのエラー修正

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,11 +1,11 @@
 import { Metadata } from 'next';
-import Pagination from '@/app/ui/customers/pagination';
-import Search from '@/app/ui/search';
-import Table from '@/app/ui/invoices/table';
-import { CreateInvoice } from '@/app/ui/invoices/buttons';
-import { lusitana } from '@/app/ui/fonts';
-import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
-import { Suspense } from 'react';
+// import Pagination from '@/app/ui/customers/pagination';
+// import Search from '@/app/ui/search';
+// import Table from '@/app/ui/invoices/table';
+// import { CreateInvoice } from '@/app/ui/invoices/buttons';
+// import { lusitana } from '@/app/ui/fonts';
+// import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
+// import { Suspense } from 'react';
 
 export const metadata: Metadata = {
   title: 'Customers',
